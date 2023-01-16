eb668ef1dac827c1646cfd77217c558a2c16809d
Code Heart <119617297+Vpan23@users.noreply.github.com>
2023-01-16 07:50:52 -0500

    Add files via upload

---
+++ C++/problemas.cpp
@@ -0,0 +1,49 @@
+//1. Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//	char name;
+//	cout<<"Write your name: ";
+//	cin>>name;
+//		cout<<"\n";
+//	cout<<"Hello "<<name<<"How are you?;
+//
+//	return 0;
+//}
+
+//2. Calcular el cambio de monedas en dólares y euros al ingresar cierta cantidad de dinero en
+//pesos.
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//	float pesos,dolar,euro;
+//	cout<<"Put your amount of money in Pesos: ";
+//	cin>>pesos;
+//	dolar = 0.053 * pesos;
+//	euro = 0.049 * pesos;
+//	cout<<"\n";
+//	cout<<"Your present balance in dolar is: "<<dolar<<" and in euro: "<<euro;
+//	return 0;
+//}
+
+//3. Hacer un programa que calcule el salario de un empleado, si se descuenta el 20% de su
+//salario bruto.
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main(){
+//	
+//	float salary,desc,euro;
+//	cout<<"Give me your salary of your work: ";
+//	cin>>salary;
+//	salary =  salary - (salary*0.2);
+//		cout<<"\n";
+//	cout<<"Your Salary Discounted is: "<<salary;
+//	
+//	return 0;
+//}
